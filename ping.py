fcb0afb5d9d229e831a2c7a8da2e1bdea845836c
Juan Manuel Lambre <juanmlambre@gmail.com>
2018-08-30 19:24:53 -0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,35 +1,19 @@
 from flask import Flask
-import RPi.GPIO as GPIO
 
-
-OUT_PINS = [24]
 app = Flask(__name__)
 
 
 @app.route('/')
 def ping():
-    return "Ping pong!"
+    return "Hello world :)"
 
-@app.route('/led/<ledNo>/on')
-def ledOn(ledNo):
-    GPIO.output(int(ledNo), GPIO.HIGH)
-    return "LED " + ledNo + " is on"
-
-@app.route('/led/<ledNo>/off')
-def ledOff(ledNo):
-    GPIO.output(int(ledNo), GPIO.LOW)
-    return "LED " + ledNo + " is off"
-
-
-def setup():
-    GPIO.setmode(GPIO.BCM)
-    for pin in OUT_PINS:
-        GPIO.setup(pin, GPIO.OUT)
+@app.route('/ping')
+def ping():
+    return "Pong!"
 
 
 def main():
-    setup()
-    app.run(host='0.0.0.0', port=4804)
+    app.run(host='0.0.0.0', port=8088)
 
 
 if __name__ == "__main__":
